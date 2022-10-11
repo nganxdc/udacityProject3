e2bbcc44a720c80e52c9cbfbbf851b011b73a374
nganxdc <nganxdc@gmai.com>
2022-10-11 23:20:52 +0700

    finish 1

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9e94eeb27e6449b8bbbf66f506738b3-435259342.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
